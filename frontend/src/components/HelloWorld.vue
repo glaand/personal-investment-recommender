af172d55755013c0aa8576a351464bebcccd5c90
André Glatzl <andre@alpinesoft.ch>
2023-10-07 16:54:47 +0000

    MVP Frontend done

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,38 +1,56 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <v-row>
+        <v-col>
+          <v-expansion-panels variant="accordion">
+            <v-expansion-panel-title hide-actions color="orange">Something Similar</v-expansion-panel-title>
+            <v-expansion-panel
+              v-for="i in 3"
+              :key="i"
+              color="orange-lighten-3"
+              title="Nvidia"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            ></v-expansion-panel>
+          </v-expansion-panels>
+        </v-col>
+        <v-col>
+          <v-expansion-panels variant="accordion">
+            <v-expansion-panel-title hide-actions color="blue">Something Essential</v-expansion-panel-title>
+            <v-expansion-panel
+              v-for="i in 3"
+              :key="i"
+              color="blue-lighten-3"
+              title="Nestlé"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            ></v-expansion-panel>
+          </v-expansion-panels>
+        </v-col>
+        <v-col>
+          <v-expansion-panels variant="accordion">
+            <v-expansion-panel-title hide-actions color="green">Something Special</v-expansion-panel-title>
+            <v-expansion-panel
+              v-for="i in 3"
+              :key="i"
+              color="green-lighten-3"
+              title="Flughafen Zürich"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            ></v-expansion-panel>
+          </v-expansion-panels>
+        </v-col>
+      </v-row>
+      <v-row align="center" justify="center">
+          <v-col cols="auto">
+            <v-btn color="primary" text prepend-icon="mdi-face-agent">Mit Berater absprechen</v-btn>
+          </v-col>
+          <v-col cols="auto">
+            <v-btn color="primary" text variant="outlined" prepend-icon="mdi-close">Schlechte empfehlungen...</v-btn>
+          </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<script lang="ts" setup>
+  //
+</script>
